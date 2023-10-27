9950b9a12582801ad7cfd9a3e92817eacc00f298
aram1l7 <arammartirosyann16@gmail.com>
2023-10-28 00:50:17 +0400

    Added readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# Blog frontend
+
+## Demo
+
+[Project demo](https://arams-blog.vercel.app/)
+
+### Installation
+
+```
+git clone git@github.com:aram1l7/blog-frontend.git
+
+yarn install
+yarn start
+
+```
+
+## Environment Variables
+
+`REACT_APP_DEV_API_URL=http://localhost:4000/api/blog`
+`REACT_APP_PROD_API_URL=https://aram-blog-server.vercel.app/api/blog`
--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const { NODE_ENV, REACT_APP_DEV_API_URL, REACT_APP_PROD_API_URL } = process.env;
 
 const apiUrl =
-  (process.env.NODE_ENV === "development" ? REACT_APP_DEV_API_URL : REACT_APP_PROD_API_URL) ?? "";
+  (NODE_ENV === "development" ? REACT_APP_DEV_API_URL : REACT_APP_PROD_API_URL) ?? "";
 
 
 export const getPosts = () => {
